081640402cbff4a64f09bd0a0f97f2f95a9696bb
jyt6640 <jyt6640@gmail.com>
2026-02-06 16:57:58 +0900

    feat: 로그인 컨트롤러 기능 구현

--- src/main/java/com/seolstudy/backend/domain/auth/controller/AuthController.java
+++ src/main/java/com/seolstudy/backend/domain/auth/controller/AuthController.java
@@ -1,14 +1,18 @@
 package com.seolstudy.backend.domain.auth.controller;
 
+import com.seolstudy.backend.domain.auth.dto.LoginRequest;
+import com.seolstudy.backend.domain.auth.dto.LoginResponse;
 import com.seolstudy.backend.domain.auth.dto.SignUpRequest;
 import com.seolstudy.backend.domain.auth.dto.SignUpResponse;
 import com.seolstudy.backend.domain.auth.service.AuthService;
 import com.seolstudy.backend.global.payload.CommonResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +23,16 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED)
     public CommonResponse<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest request){
         SignUpResponse response = authService.signUp(request);
+        return CommonResponse.onSuccess(response);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public CommonResponse<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse response = authService.login(request);
         return CommonResponse.onSuccess(response);
     }
 }
