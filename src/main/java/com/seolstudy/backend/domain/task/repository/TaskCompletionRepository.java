d3c451cc07646fb2d2200c2f03f94e6baff697f2
sumi-03 <lsmkoo03@naver.com>
2026-02-08 18:00:56 +0900

    feat: 과제 완료 조회 메서드 추가

--- src/main/java/com/seolstudy/backend/domain/task/repository/TaskCompletionRepository.java
+++ src/main/java/com/seolstudy/backend/domain/task/repository/TaskCompletionRepository.java
@@ -3,9 +3,11 @@ package com.seolstudy.backend.domain.task.repository;
 import com.seolstudy.backend.domain.task.entity.TaskCompletion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskCompletionRepository extends JpaRepository<TaskCompletion, Long> {
     boolean existsByTaskId(Long taskId);
     Optional<TaskCompletion> findByTaskId(Long taskId);
+    List<TaskCompletion> findByTaskIdIn(List<Long> taskIds);
 }
